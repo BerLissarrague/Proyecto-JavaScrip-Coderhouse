3da887caa5c4737633b838dd49078494f06974ed
Bernardo Lissarrague <berlissarrague@gmail.com>
2021-08-20 09:28:47 -0300

    .

--- inicio.js
+++ inicio.js
@@ -1,55 +1,65 @@
 const cantTurnos = " siete (7) turnos";
 const cerrado = "20 hs.";
-var miNumero = 1;
-var cantidadPersonasPromedio = 0;
-var acumuladorEdades = 0;
-var bienvenido = ("Bienvenido, gracias por su visita");
+let miNumero = 1;
+let cantidadPersonasPromedio = 0;
+const cliente = [];
+const cantidadturnos = 7;
+acumuladorEdades = 0;
+/*class DescripCliente {
+    constructor(nombre, edad, turnoingresado) {
+        this.nombre,
+            this.edad,
+            this.turnoingresado
+    }
+}*/
+let bienvenido = ("Bienvenido, gracias por su visita");
 alert(bienvenido);
 do {
     cantidadPersonasPromedio = parseInt(prompt("Ingrese cantidad de turnos que desea sacar (distinto de 0)"));
 } while (cantidadPersonasPromedio == 0);
 for (miNumero; miNumero <= cantidadPersonasPromedio; miNumero++) {
-    var edad = parseInt(prompt("Ingrese su edad"));
-    if (edad > 0 && edad < 110) {
-        if (edad < 18) {
-            alert("Recorda venir con un mayor");
-        }
-        acumuladorEdades = acumuladorEdades + edad;// acumuladorEdades += edad; contador += 1;
-        console.log("La suma de las edades es: ", acumuladorEdades);
-        alert("Turno 1 de 8 a 9:30" + " Turno 2 de 9:30 a 11 " + "Turno 3 de 11 a 12:30" + " Turno 4 de 12:30 a 14 " + "Turno 5 de 14 a 15:30" + " Turno 6 de 15:30 a 17 " + "Turno 7 de 17 a 18:30");
-        var turno = parseInt(prompt("Ingresa el numero turno que desee"));
-        let turnoDado = 0;
-        if (turno >= 1 && turno <= 7) {
-            if (turno == turnoDado) {
-                do {
-                    alert("El turno esta dado, intenta otro turno");
-                    var turno = parseInt(prompt("Ingresa el numero del turno"));
-                } while (turno != turnoDado) {
-                    alert("turno Registrado");
-                    console.log("Su turno es el numero ", turno);
-                }
+    let nombre = prompt("ingresa tu nombre");
+    let edad = parseInt(prompt("Ingrese su edad"));
+    if (edad < 18) {
+        alert("Recorda venir con un mayor");
+    }
+    acumuladorEdades = acumuladorEdades + edad;
+    console.log("La suma de las edades es: ", acumuladorEdades);
+    alert("Turno 1 de 8 a 9:30 Turno 2 de 9:30 a 11  Turno 3 de 11 a 12:30 Turno 4 de 12:30 a 14  Turno 5 de 14 a 15:30  Turno 6 de 15:30 a 17  Turno 7 de 17 a 18:30");
+    turnoIngresado = parseInt(prompt("Ingresa el numero turno que desee"));
+    cliente.push({ nombre, edad, turnoIngresado });
+    console.log(cliente)
+    turnoDado = cliente.some(turnoIngresado => cliente.turnoIngresado === turnoIngresado);
+    console.log(turnoDado);
+    if (turnoIngresado >= 1 && turnoIngresado <= 7) {
+        if (turnoDado) {
+            do {
+                alert("El turno esta dado, intenta otro turno");
+                let turno = parseInt(prompt("Ingresa el numero del turno"));
+            } while (turnoingresado != turnoDado) {
+                alert("turno Registrado");
+                console.log("Su turno es el numero ", turnoIngresado);
             }
-            else {
-                alert("turno registrado");
-                console.log("Su turno es el numero ", turno);
-            }
-
         }
-        else if (turno < 1 || turno > 7) {
-            alert("Solo se toman " + cantTurnos + " por dia");
-        }
-        else if (isNaN(turno)) {
-            alert('El turno no se ha guardado ya que no especificó horario o canceló la transacción')
+        else {
+            alert("turno registrado");
+            console.log("Su turno es el numero ", turnoIngresado);
         }
 
     }
+    else if (turnoIngresado < 1 || turnoIngresado > 7) {
+        alert("Solo se toman " + cantTurnos + " por dia");
+    }
+    else if (isNaN(turnoIngresado)) {
+        alert('El turno no se ha guardado ya que no especificó horario o canceló la transacción');
+    }
+
+    if (edad > 110) {
+        alert('Edad inválida');
+    }
 }
-if (edad > 110) {
-    alert('Edad inválida')
-}
-let promedio = acumuladorEdades / cantidadPersonasPromedio;
+console.log("Ellos son las personas que tienen turno para pasar por el estudio : ", cliente)
+promedio = acumuladorEdades / cantidadPersonasPromedio;
 console.log('el promedio de edad es: ', promedio);
-
-
 
 
